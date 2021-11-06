5ff30cac5708e2e306c34b41929414fe666c886a
Артем <kysapka@gmail.com>
2021-11-06 23:14:53 +0300

    css styled fix

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import style from './Main.module.scss';
 import styleContainer from '../common/styles/Container.module.css'
+import photoImg from '../assets/image/photo.png'
+
+const photo = {
+    backgroundImage: `url(${photoImg})`
+};
 
 export const Main = () => {
     return (
         <div className={`${style.mainBlock} global`}>
             <div className={styleContainer.container}>
                 <div className={style.text}>
-                    <span>Hi there</span>
-                    <h1>I am Artem</h1>
+                    <span>Hi there,</span>
+                    <h1>I am Artem Kryuchkov</h1>
                     <p>Frontend Developer.</p>
                 </div>
-                <div className={style.photo}></div>
+                <div className={style.photo} style={photo}></div>
             </div>
         </div>
     );
