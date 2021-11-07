546df61543c6f7c1f746d1aa843d2bb29343a084
Артем <kysapka@gmail.com>
2021-11-08 01:56:05 +0300

    contacts block

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import style from './Contacts.module.css'
+import style from './Contacts.module.scss'
 import styleContainer from "../common/styles/Container.module.css";
+import {ProjectButton} from "../myprojects/myproject/MyProject";
+import {Title} from "../common/components/title/Title";
 
 export const Contacts = () => {
     return (
         <div className={`${style.contactsBlock} global`}>
             <div className={`${styleContainer.container} ${style.contactsContainer}`}>
-                <h2 className={style.title}>Контакты</h2>
+
+                {/*<h2 className={style.title}>Контакты</h2>*/}
+                <Title text={"Contacts"} />
+
                 <form className={style.contactForm}>
-                    <input type={"text"} />
-                    <input type={"text"} />
-                    <textarea style={{resize: "none"}}/>
-                <button type={"submit"} className={style.contactsButton}>Отправить</button>
+                    <input type={"text"} placeholder="Enter your name"/>
+                    <input type={"text"} placeholder="Enter your e-mail"/>
+                    <textarea style={{resize: "none"}} placeholder="Enter your message"/>
+                    <ProjectButton text={"Send message"}/>
+                {/*<button type={"submit"} className={style.contactsButton}>Отправить</button>*/}
                 </form>
             </div>
         </div>
